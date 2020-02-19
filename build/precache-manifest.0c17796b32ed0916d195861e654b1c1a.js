2d5df7e8fc83d0e83699b0e3e3184e526b4062e8
Jimoh Abdulrahman <39130903+abdol4real@users.noreply.github.com>
2020-02-19 15:33:06 +0100

    ...

---
+++ build/precache-manifest.0c17796b32ed0916d195861e654b1c1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26c40ca08fbf26af93c732c0829433eb",
+    "url": "/ToDO/index.html"
+  },
+  {
+    "revision": "60eb09dac9eb505cd21e",
+    "url": "/ToDO/static/css/main.86f5cbe9.chunk.css"
+  },
+  {
+    "revision": "e338bbb9cc7c7db0be3f",
+    "url": "/ToDO/static/js/2.07d09b33.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/ToDO/static/js/2.07d09b33.chunk.js.LICENSE"
+  },
+  {
+    "revision": "60eb09dac9eb505cd21e",
+    "url": "/ToDO/static/js/main.94cac727.chunk.js"
+  },
+  {
+    "revision": "9427a3e4bfdeea0821b9",
+    "url": "/ToDO/static/js/runtime-main.52447225.js"
+  }
+]);
